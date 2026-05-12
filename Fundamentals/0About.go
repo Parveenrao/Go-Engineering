4f62e07899271fd882a0b9dc19d4513301bf5165
Parveenrao <parveeenrao7727@gmail.com>
2026-05-12 21:12:12 +0530

    Intro Go

---
+++ Fundamentals/0About.go
@@ -0,0 +1,15 @@
+/* 
+
+=> Go is a statically typed language which means type are determined at compile time rather than runtime .
+
+   Compiler check correctness before program excutes , helping catch errors early and improving performance
+
+=> Go is a compiled language where the source code is compiled into a native executable binary before runtime. 
+   
+   The binary can run directly on the operating system without needing an interpreter or virtual machine.   
+
+
+
+
+
+*/
